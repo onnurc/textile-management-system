dbaec7fb29abd7bac7da5147039982ad1a126627
onnurc <onurcetinkaya149@gmail.com>
2026-02-14 22:23:10 +0000

    Initial commit

---
+++ src/test/java/com/tekstil/textile_management_system/TextileManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tekstil.textile_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextileManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
