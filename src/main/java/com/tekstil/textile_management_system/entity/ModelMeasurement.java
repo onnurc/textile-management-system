0db29a78999afb447198ca5831590f9408d8499c
onnurc <onurcetinkaya149@gmail.com>
2026-05-21 01:25:48 +0300

    fix: separate .env and application-aws.properties in gitignore

---
+++ src/main/java/com/tekstil/textile_management_system/entity/ModelMeasurement.java
@@ -0,0 +1,4 @@
+package com.tekstil.textile_management_system.entity;
+
+public class ModelMeasurement {
+}
---
+++ src/main/java/com/tekstil/textile_management_system/exception/BadRequestException.java
@@ -0,0 +1,4 @@
+package com.tekstil.textile_management_system.exception;
+
+public class BadRequestException {
+}
